bc5efec159326d8c77be45ba5c6c81f937fa721f
fersayago <sayago.fernando@gmail.com>
2022-09-24 19:30:28 -0300

    Escribiendo HTML desde

--- 02.Manipulacion-de-DOM/script.js
+++ 02.Manipulacion-de-DOM/script.js
@@ -12,12 +12,25 @@ const parrafoConId = document.getElementById('parrafo-con-id');
 
 const input = document.querySelector('input');
 
-console.log({
-  h1,
-  parrafo,
-  parrafoConClase,
-  parrafoConId,
-  input
-})
+console.group('Modificación de atributos del h1')
+console.log(h1.getAttribute('curso')) // platzi
+console.log(h1.getAttribute('class')) // verde
+h1.setAttribute('class', 'rojo')
+console.log(h1.getAttribute('class')) // rojo
+h1.classList.add('verde');
+console.log(h1.getAttribute('class')) // rojo verde
+h1.classList.remove('rojo');
+console.log(h1.getAttribute('class')) // verde
+h1.classList.toggle('rojo'); // prende o apaga dependiendo si tiene o no
+console.log(h1.getAttribute('class')) // verde rojo
+console.groupEnd()
 
-console.log(input.value);
+
+// Creación de elementos mediante JS
+
+const img = document.createElement('img');
+img.setAttribute('src', 'https://placeimg.com/200/200/animals');
+
+const parrafoConIdVacio = document.getElementById('parrafo-con-id-vacio');
+
+parrafoConIdVacio.append(img);
