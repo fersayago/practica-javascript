f1811ffca868d2624deae7b19d9f7e03d445cd6d
fersayago <sayago.fernando@gmail.com>
2022-09-24 13:27:35 -0300

    Leyendo HTML desde Javascript

--- 02.Manipulacion-de-DOM/script.js
+++ 02.Manipulacion-de-DOM/script.js
@@ -1,1 +1,23 @@
-console.log("Hello desde archivo script.js")
+// console.log("Hello desde archivo script.js")
+
+const h1 = document.querySelector('h1');
+
+const parrafo = document.querySelector('p');
+
+// const parrafoConClase = document.querySelector('.parrafo-con-clase')
+const parrafoConClase = document.getElementsByClassName('parrafo-con-clase');
+
+// const parrafoConId = document.querySelector('#parrafo-con-id')
+const parrafoConId = document.getElementById('parrafo-con-id');
+
+const input = document.querySelector('input');
+
+console.log({
+  h1,
+  parrafo,
+  parrafoConClase,
+  parrafoConId,
+  input
+})
+
+console.log(input.value);
