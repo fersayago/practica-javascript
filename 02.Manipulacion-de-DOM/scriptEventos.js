6815e867078fc279065cbe3b187c993c0480b5c0
fersayago <sayago.fernando@gmail.com>
2022-09-24 20:13:20 -0300

    addEventListener

--- 02.Manipulacion-de-DOM/scriptEventos.js
+++ 02.Manipulacion-de-DOM/scriptEventos.js
@@ -1,10 +1,15 @@
 const h1 = document.querySelector('h1');
+const form = document.getElementById('form');
 const input1 = document.querySelector('#calculo1');
 const input2 = document.querySelector('#calculo2');
 const btn = document.querySelector('#btn-calcular');
 const parrafoResultado = document.getElementById('parrafo-resultado')
 
-function btnOnClick(){
+form.addEventListener('submit', sumarInputValues);
+
+function sumarInputValues(event){
+  console.log(event)
+  event.preventDefault();
   const valor1 = input1.value;
   const valor2 = input2.value;
   const resultado = parseInt(valor1) + parseInt(valor2);
